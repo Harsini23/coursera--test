cdef43a48e693bdfd1448142d7bc169b558874fb
Harsini23 <harsini.ramalingam@gmail.com>
2020-07-13 16:28:52 +0530

    using javascript

--- mod4-solution/SpeakGoodbye.js
+++ mod4-solution/SpeakGoodbye.js
@@ -14,8 +14,8 @@
 (function(window){
 var byeSpeaker ={};
 var speakWord = "Good Bye";
-
-byeSpeaker.speak =function () {
+name=byeSpeaker.speak;
+byeSpeaker.speak =function (name) {
   console.log(speakWord + " " + name);
 }
 
